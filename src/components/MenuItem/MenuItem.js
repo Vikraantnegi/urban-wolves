81306c41136aa419d844b337ad766de3c30fa0b5
vikrant <vikrant.negi74@gmail.com>
2022-02-09 02:39:22 +0530

    Styling menu fixes

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import './MenuItem.scss'
 
 const MenuItem = (props) => {
     const { title, image, link, customClass } = {...props};
     return (        
-        <div 
-            style={{ backgroundImage: `url(${image})` }} 
+        <div
             className={`${customClass} menu-item`} 
             onClick={() => window.open(link)}
         >
+            <div
+                className='bg-image' 
+                style={{ backgroundImage: `url(${image})` }}
+            />
             <div className='content'>
                 <div className='title'>{title}</div>
                 <span className='subtitle'>Shop Now</span>
