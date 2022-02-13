261de96ecbd6e941125a6373a90bb89f28467b87
vikrant <vikrant.negi74@gmail.com>
2022-02-13 14:55:55 +0530

    Google User storage

--- src/App.js
+++ src/App.js
@@ -6,15 +6,25 @@ import HatsPage from './pages/HatsPage/HatsPage';
 import ShopPage from './pages/ShopPage/ShopPage';
 import Header from './components/HomePage/Header/Header';
 import AuthPage from './pages/AuthPage/Authpage';
-import { auth } from './firebase'
+import { auth, createUser } from './firebase'
 
 function App() {
 
   const [ user, setUser ] = useState(null);
 
   useEffect(() => {
-    auth.onAuthStateChanged(user => {
-      setUser(user)
+    auth.onAuthStateChanged( async user => {
+      if(user){
+        const userRef = await createUser(user);
+        userRef.onSnapshot(snapshot => {
+          setUser({
+            id: snapshot.id,
+            ...snapshot.data()
+          })
+        })
+      } else{
+        setUser(null)
+      }
     })
   }, [])
 
--- src/firebase.js
+++ src/firebase.js
@@ -11,6 +11,31 @@ const firebaseConfig = {
   appId: "1:1029185855183:web:883a4598fd6efeb552e1ba"
 };
 
+export const createUser = async (userAuth, data) => {
+  if(!userAuth) return;
+  
+  const userRef = fireStore.doc(`users/${userAuth.uid}`)
+  const userSnap = await userRef.get();
+
+  if(!userSnap.exists){
+    const { displayName, email, photoUrl } = userAuth;
+    const createdTime = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        photoUrl,
+        createdTime,
+        ...data
+      })
+    } catch(error) {
+      console.log('error creating user', error.message)
+    }
+  }
+
+}
+
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
