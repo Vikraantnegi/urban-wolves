b69b215ef9751bd3018738606bf006364a1f349f
vikrant <vikrant.negi74@gmail.com>
2022-02-17 19:16:11 +0530

    Cart Item

--- src/components/HomePage/Header/CartDropDown/CartDropdown.js
+++ src/components/HomePage/Header/CartDropDown/CartDropdown.js
@@ -2,15 +2,24 @@ import React from 'react'
 import { connect } from 'react-redux'
 import CustomButton from '../../../AuthPage/CustomButton/CustomButton'
 import './CartDropdown.scss'
+import CartItem from './CartItem.js/CartItem'
 
 const CartDropdown = (props) => {
   const { cartItems } = {...props}
   return (
     <div className='cart-dropdown'>
         <div className='cart-items'>
+          {
+            cartItems.map(item => (
+              <CartItem
+                key={item.id}
+                item={item}
+              />
+            ))
+          }
         </div>
         <CustomButton type='button'>Checkout</CustomButton>
-    </div>
+    </div> 
   )
 }
 
