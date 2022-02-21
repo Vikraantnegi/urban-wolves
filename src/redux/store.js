de09b7da2cad64575e851d432b61880706908f42
vikrant <vikrant.negi74@gmail.com>
2022-02-21 20:14:52 +0530

    Production

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import rootReducer from "./rootReducer";
 import logger from "redux-logger";
 import persistStore from "redux-persist/es/persistStore";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger)
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 export const persistor = persistStore(store);
