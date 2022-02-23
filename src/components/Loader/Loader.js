35c17f0444c17e798081dbcc4b6df2469dafca94
vikrant <vikrant.negi74@gmail.com>
2022-02-23 21:52:02 +0530

    Restructer Loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Loader.scss';
 
-const Loader = WrappedComponent => ({ isLoading, ...otherProps }) => {
-    return isLoading ? (
-        <div className='spinner-overlay'>
-            <div className='spinner-container' />
-        </div>
-    ) : <WrappedComponent {...otherProps} />
+const Loader = WrappedComponent => {
+    const Spinner = ({ isLoading, ...otherProps }) => {
+        return isLoading ? (
+            <div className='spinner-overlay'>
+                <div className='spinner-container' />
+            </div>
+        ) : <WrappedComponent {...otherProps} />
+    }
+    
+    return Spinner;
 }
 
 export default Loader;
